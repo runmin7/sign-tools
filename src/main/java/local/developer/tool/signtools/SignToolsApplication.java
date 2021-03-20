3f6829cca0ead2c71020a351ffa796bc475408e9
minho <runmin@naver.com>
2021-03-20 15:48:49 +0900

    init

---
+++ src/main/java/local/developer/tool/signtools/SignToolsApplication.java
@@ -0,0 +1,13 @@
+package local.developer.tool.signtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignToolsApplication {
+
+//	public static void main(String[] args) {
+//		SpringApplication.run(SignToolsApplication.class, args);
+//	}
+
+}
---
+++ src/test/java/local/developer/tool/signtools/SignToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package local.developer.tool.signtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
